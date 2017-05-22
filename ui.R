b52901dd8becb7f726b7ff1e21ad58fb035bd44c
spindler <spindler>
2017-05-22 18:21:05 +0200

    Intro Tab and Formulas (preliminary)

--- ui.R
+++ ui.R
@@ -20,22 +20,22 @@ ui <- shinyUI(fluidPage(
     ########################
 
     tabPanel("Introduction", value = "intro",
-        h2("Welcome!", align = "center"),
-        br(),
-        p("This Shiny App helps you to learn more about the Time-Window
-          Ingration Model (TWIN), developed by Hans Colonius, Adele
-          Diederich, and colleagues", align = "center",
-        a("(Colonius & Diederich, 2004).",
-          href="https://www.uni-oldenburg.de/fileadmin/user_upload/psycho/ag/kogn/colonius/Jcogn.pdf",
-          target="_blank")),
-        p("It allows you to simulate and estimate the parameteres and even
-          to upload your own data file.", align = "center"),
-        p("Feel free to explore the app and navigate through the tabs!",
-          align ="center"),
-        br(),
-        h5(strong("This Shiny app is based on the app by Annika
-                  Thierfelder, and was extended by:"), align = "center"),
-        br(),
+             h2("Welcome!", align = "center"),
+             br(),
+             p("This Shiny App helps you to learn more about the Time-Window
+               Integration Model (TWIN), developed by Hans Colonius, Adele
+               Diederich, and colleagues", align = "center",
+               a("(Colonius & Diederich, 2004).",
+                 href="https://www.uni-oldenburg.de/fileadmin/user_upload/psycho/ag/kogn/colonius/Jcogn.pdf",
+                 target="_blank")),
+             p("It allows you to simulate and estimate the model parameters either from 
+               virtual data or from your own datafile.", align = "center"),
+             p("If you have any questions or remarks, please send an email to XXXXXXXXXXX",
+               align ="center"),
+             br(),
+             h5(strong("This Shiny app is based on the app by Annika
+                       Thierfelder, and was extended by:"), align = "center"),
+             br(),
         fluidRow(
           column(3, "Aditya Dandekar", br(),
                  img(src="bremen.png", width = "150")),
@@ -63,59 +63,115 @@ ui <- shinyUI(fluidPage(
     ##########################
 
     tabPanel("The TWIN Model", value="twinmod",
-
-        h2("Model Description"),
-
-        p("Crossmodal interaction is defined as the situation in which the
-          perception of an event as measured in terms of one modality is
-          changed in some way by the concurrent stimulation of one or more
-          other sensory modalities (Welch & Warren, 1986)."),
-        p("Multisensory integration is defined as the (neural) mechanism
-          underlying crossmodal interaction."),
-        p("The Time-Window of Integration Model (TWIN) postulates that a
-          crossmodal stimulus triggers a race mechanism among the
-          activations in very early, peripheral sensory pathways. This
-          first stage is followed by a compound stage of converging
-          subprocesses that comprise neural integration of the input and
-          preparation of a response. The second stage is defined by
-          default: It includes all subsequent processes that are not part
-          of the peripheral processes in the first stage (Diederich,
-          Colonius, & Kandil, 2016)."),
-        p(img(src="modeltwin.png", width = "550"), align = "center"),
-        p("At the behavioral level, multisensory integration translates
-          itself into faster reaction times, higher detection
-          probabilities, and improved discrimination."),
-        p("At the neural level, multisensory integration translates itself
-          into an increased total number of responses, as well as shorter
-          response latencies."),
-        p("In the Focused Attention Paradigm (FAP), two or more stimuli are
-          presented simultaneously or with a short delay between the stimuli.
-          The participant is asked to respond as quickly as possible to a
-          stimulus of a pre-defined modality (target) and ignore the other
-          stimulus (non-target modality)."),
-        p(img(src="fap.jpeg", width = "500", align = "center")),
-        p("In the Redundant Signals Paradigm (RSP), two or more stimuli
-          are also presented simultanously or with a short delay between
-          the stimulus. However, the participant is asked to respond to
-          a stimulus of any modality detected first."),
-        p(img(src="rtp.jpeg", width = "500", align = "center")),
-
-        h2("Model Equations"),
-
-        h4("Requirement for multisensory integration"),
-        img(src="itw.jpeg", width = "300"),
-        h4("Mean reaction times in the unimodal and bimodal conditions"),
-        img(src="rts.jpeg", width = "300"),
-        h4("Observable reaction time following the logic of the model"),
-        img(src="rt.jpeg", width = "400"),
-        h4("Objective function"),
-        img(src="objfun.jpeg", width = "300"),
-        br(),
-        h4("Three cases for the probability of integration P(I)"),
-        img(src="p1.jpeg", width = "400", align = "left"),
-        img(src="p2.jpeg", width = "400", align = "left"),
-        img(src="p3.jpeg", width = "400", align = "left")
-      ),
+             
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput("topic", "Select the topic:",
+                             choices = c("The TWIN model" = "twinmod",
+                                         "Focused Attention Paradigm" = "fap",
+                                         "Redundant Target Paradigm" = "rtp")),
+                 a("See to TWIN publication (Kandil, Diederich & Colonius, 2014)",
+                   href="http://jov.arvojournals.org/article.aspx?articleid=2193864",
+                   target="_blank")
+               ),
+               
+               mainPanel(
+                 conditionalPanel(condition = ("input.topic == 'twinmod'"),
+                                  
+                                  h2("Theoretical Background"),
+                                  p("Multisensory interaction is defined as the (neural) mechanism underlying crossmodal interaction. 
+                                    Crossmodal interaction describes a situation in which the perception of a stimulus or eventof a given modality is in some way influenced
+                                    by the presence of one or more stimuli of othersensory modalities. On the behavioral level, multisensory integration is characterized by 
+                                    faster reaction times, higher detection probabilities and improved discrimination. 
+                                    On the neural level,it is observed in form of an increased number of total responses as well as shorter response latencies.(Diederich,
+                                    Colonius, & Kandil, 2016)."),
+                                  p(img(src="modeltwin.png", width = "550"), align = "center"),
+                                  p("In tasks measuring multisensory integration, participants are asked to respond to a cross-modal set of stimuli - usually visual and auditive stimuli -, 
+                                    that are presented simultaneously or at certain interstimulus intervals. The task is then either to respond to a target stimulus of acertain modality 
+                                    (usually the visual stimulus, see Focused Attention Paradigm (FAP)), or to respond to the first stimulus thatis perceived (see Redundant Target Paradigm (RTP)), by button press.
+                                    Following the TWIN model, the general processing can be split into two stages:",br(),"
+                                    In the", em("First-Stage Assumption"),",
+                                    a crossmodal (audiovisual) stimulus triggers a race mechanism in the very early, peripheral sensory pathways. Here, the pathways for different modalities are initially separated.",br()," 
+                                    In the", em("Stage-Two Assumption"),
+                                    ", all subsequent, possibly temporally overlapping, processes that are not part of the peripheral processes in the first stage are included (neural integration of the input and preparation of a response). 
+                                    Multisensory integration is then obvservable in an increase or decrease in second-stage processing time.",br(),"
+                                    At this, the", em("Time-Window-of-Integration Assumption"),"
+                                    postulates that multisensory integration can only occur if the processes of the first stage all terminate within a given time window (the time window of integration).
+                                    "),
+                                  p("Then, the amount of interaction is defined in the", em("Assumption of Temporal Separability"), "where an increase or decrease of second-stage processing time is a function of crossmodal stimulus features but does
+                                    not depend on the presentation asynchrony (SOA) of the stimuli."),
+                                  p(img(src="fap.jpeg", width = "500", align = "center")),
+                                  p(img(src="rtp.jpeg", width = "500", align = "center"))
+                                  
+                                  ),
+                 
+                 conditionalPanel(condition = ("input.topic == 'fap'"),
+                                  h2("The Focused Attention Paradigm (FAP)"),
+                                  withMathJax(p("In the Focused Attention Paradigm, one stimulus modality is pre-defined as the target stimulus to respond to. The other stimulus is called the non-target stimulus. 
+                                                In this experiment, we assume that the visual stimulus is the target modality, and the auditory stimulus is the non-target modality.
+                                                Let \\(I\\) denote the event that multisensory integration occurs. For FAP, the condition for multisensory integration is defined as  $$I_{FAP} = {A + \\tau < V < A + \\tau + \\omega}$$ with the probability of integration \\(P(I)\\).
+                                                Here, \\(\\tau\\) denotes the stimulus-onset asynchrony (SOA), which is determined by the experimental setup, and \\(\\omega\\) as the window width of integration, the time window, in which integration may occur.
+                                                A positive \\(\\tau\\) value thus indicates that that the visual stimulus is presented before the auditory, and a negative \\(\\tau\\) values indicates the reverse presentation order. In theory, \\(V\\) is always presented at \\(t = 0\\)."),
+                                              h4("The Probability of Integration"),
+                                              p("The probability of integration can be described as a function of SOA and window width:  
+                                                Seeing that the peripheral processing time for the visual (\\(V\\)) and auditory (\\(A\\)) stimulus are assumed to be exponentially distributed and statistically independent (as they refer to very early sensory processing), 
+                                                with parameters \\(\\lambda_V\\) and \\(\\lambda_A\\) and expected values of \\(1/\\lambda_V\\) and \\(1/\\lambda_A\\),
+                                                $$\\begin{align}
+                                                f_V(t) = \\lambda_{V}e^{-\\lambda_{V}t} \\\\
+                                                f_A(t) = \\lambda_{A}e^{-\\lambda_{A}t}
+                                                \\end{align}$$
+                                                for \\(t \\geq 0\\), and \\(f_V(t) = f_A(t) \\equiv 0\\) for \\(t < 0.\\) The corresponding distribution functions are \\(F_V(t)\\) and  \\(F_A(t)\\), respectively. 
+                                                The model can therefore be rewritten as:")),
+                                  withMathJax(p("$$\\begin{align}
+                                                P(I_{\\tau\\omega}) &= Pr(A + \\tau < V < A + \\tau + \\omega) \\\\
+                                                &= \\int_{0}^{\\infty} \\! f_A(x)\\{F_V(x + \\tau + \\omega) \\\\ 
+                                                &- F_V(x+\\tau)\\}\\,\\mathrm{d}x.
+                                                \\end{align}$$"
+                                  )),
+                                  withMathJax(p("Thus, the integration function for \\(P(I)\\) depends on the three cases for the sign of \\(\\omega\\) + \\(\\tau\\):")),
+                                  withMathJax(p("(i) \\(\\tau\\) < \\(\\tau\\) + \\(\\omega\\) < 0", align = "center",
+                                                "$$\\begin{align}
+                                                P(I_{\\tau,\\omega}) &= \\int_{-\\tau-\\omega}^{-\\tau} \\! \\lambda_{A}e^{-\\lambda_{A}x}{1-e^{-\\lambda_V(x+\\tau+\\omega)}} \\, \\mathrm{d}x \\\\
+                                                &-\\int_{-\\tau}^{\\infty} \\! \\lambda_{A}e^{-\\lambda_{A}x}{e^{-\\lambda_V(x+\\tau)} - e^{-\\lambda_V(x+\\tau+\\omega)}} \\, \\mathrm{d}x \\\\
+                                                &= \\frac{\\lambda_V}{\\lambda_V+\\lambda_A} e^{\\lambda_A\\tau}(-1 + e^{\\lambda_A\\omega});
+                                                \\end{align}$$
+                                                ")),
+                                  withMathJax(p("(ii)  \\(\\tau\\) < 0 < \\(\\tau\\) + \\(\\omega\\)", align = "center")),
+                                  withMathJax(p("$$\\begin{align}
+                                                P(I_{\\tau,\\omega})  &= \\int_{0}^{-\\tau} \\! \\lambda_{A}e^{-\\lambda_{A}x}\\{1-e^{-\\lambda_V(x+\\tau+\\omega)}\\} \\, \\mathrm{d}x \\\\
+                                                &+ \\int_{-\\tau}^{\\infty} \\! \\lambda_{A}e^{-\\lambda_{A}x}\\{e^{-\\lambda_V(x+\\tau)} - e^{-\\lambda_V(x+\\tau+\\omega)}\\} \\, \\mathrm{d}x \\\\
+                                                &= \\frac{1}{\\lambda_V+\\lambda_A} \\{\\lambda_A(1-e^{-\\lambda_V(\\omega+\\tau)})+ \\lambda_V(1-e^{\\lambda_A\\tau})\\};
+                                                \\end{align}$$")),
+                                  withMathJax(p("(iii) \\(0 < \\tau < \\tau + \\omega\\)", align = "center")),
+                                  withMathJax(p("$$\\begin{align}
+                                                P(I_{\\tau,\\omega}) &= \\int_{0}^{\\infty} \\! \\lambda_{A}e^{-\\lambda_{A}x}\\{e^{-\\lambda_V(x+\\tau)}-e^{-\\lambda_V(x+\\tau+\\omega)}\\} \\mathrm{d}x \\\\
+                                                &= \\frac{\\lambda_A}{\\lambda_V+\\lambda_A} \\{e^{-\\lambda_V(\\tau)}-e^{-\\lambda_V(\\omega+\\tau)}.
+                                                \\end{align}$$")),
+                                  h4("Reaction time analysis"),
+                                  withMathJax(p("Reaction times are evaluated for the unimodal condition to compare against the crossmodal condition.
+                                                Let \\(M1\\) and \\(M2\\) denote the random processing time for stage 1 and 2, so that the overall processing time of the crossmodal reaction time becomes
+                                                $$RT_{VA} = M1 + M2$$.
+                                                Expected reaction times for the unimodal condition: 
+                                                $$\\begin{align}
+                                                E[RT_V] = \\tfrac{1}{\\lambda_V} + \\mu 
+                                                \\end{align}$$
+                                                Expected Reaction times for the crossmodal condition:
+                                                $$\\begin{align}
+                                                E[RT_{VA}] = \\tfrac{1}{\\lambda_V} + \\mu - P(I) \\cdot \\Delta 
+                                                \\end{align}$$
+                                                where \\(\\mu\\) is the mean processing time of stage two."))
+                                  ),
+                 
+                 conditionalPanel(condition = ("input.topic == 'rtp'"),
+                                  h2("The Redundant Target Paradigm (RTP)"),
+                                  withMathJax(p("For RTP, the following condition for multisensory integration $$I_{RTP} = {max(V,A + \\tau) < min(V,A + \\tau) + \\omega}$$ 
+                                                with the probability of Integration \\(P(I)\\) holds.
+                                                ")),
+                                  h4("Probability of Integration"),
+                                  p("Probalility of Integration for cases, in which the visual stimulus is presented first (e.g., SOAs of 0, 50 and 100 ms)....
+                                    Probalility of Integration for cases, in which the auditory stimulus is presented first (e.g., SOAs of -100, -50 and 0 ms)...")
+                                  
+                                  )))),
 
     ######################
     ### Parameters Tab ###
