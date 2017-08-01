8725dab9f1c8dc3ea9dc87de7bb7a027db7fa889
Katharina Naumann <katharina.naumann@uni-tuebingen.de>
2017-08-01 14:28:22 +0200

    Fix call to prediction function

--- plotHelpers.R
+++ plotHelpers.R
@@ -4,7 +4,7 @@ plotEstPred.fap <- function(data, est) {
     soa <- as.numeric(sub("neg", "-", sub("SOA.", "", colnames(data))))
 
     # predict RTs from estimations
-    pred <- predict.rt(par = est$est$par, column.names = colnames(data))
+    pred <- predict.rt.fap(par = est$est$par, column.names = colnames(data))
 
     # mean and SE of observed reaction times
     obs <- colMeans(data)
