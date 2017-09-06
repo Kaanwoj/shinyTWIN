4eb6ddf64d7ee447fb6ef39fc6613c850942485b
Katharina Naumann <katharina.naumann@uni-tuebingen.de>
2017-09-06 17:37:13 +0200

    Change size of graphics to be dynamic

--- ui/ui_Team.R
+++ ui/ui_Team.R
@@ -4,23 +4,23 @@ tabPanel("Team", value = "Team",
     br(),
     fluidRow(
         column(3, "Aditya Dandekar", br(),
-               img(src="bremen.png", width = "150")),
+               img(src="bremen.png", width = "50%")),
         column(3, "Amalia Gomoiu", br(),
-               img(src="glasgow.png", width = "150")),
+               img(src="glasgow.png", width = "50%")),
         column(3, "António Fernandes", br(),
-               img(src="lisbon.png", width = "200")),
+               img(src="lisbon.png", width = "60%")),
         column(3, "Katharina Dücker", br(),
-               img(src="oldenburg.png", width = "110", height = "90"))
+               img(src="oldenburg.png", width = "40%"))
     ),
     fluidRow(
       column(3, "Katharina Naumann", br(),
-             img(src="tubingen.png", width = "150")),
+             img(src="tubingen.png", width = "50%")),
       column(3, "Martin Ingram", br(),
-             img(src="glasgow.png", width = "150")),
+             img(src="glasgow.png", width = "50%")),
       column(3, "Melanie Spindler", br(),
-             img(src="oldenburg.png", width = "110", height = "90")),
+             img(src="oldenburg.png", width = "40%")),
       column(3, "Silvia Lopes", br(),
-             img(src="lisbon.png", width = "200"))
+             img(src="lisbon.png", width = "60%"))
     ),
     br(),
     h5(strong("under supervision by Prof. Dr. Hans Colonius of the University of
