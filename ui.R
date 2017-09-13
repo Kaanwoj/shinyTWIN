7c71cb05a1a1bc3c6ef1f1497177223edbf5778e
Katharina Naumann <katharina.naumann@uni-tuebingen.de>
2017-09-13 15:30:48 +0200

    Update Introduction tab

--- ui.R
+++ ui.R
@@ -35,34 +35,40 @@ shinyUI(
       fluidRow(
         column(3, offset=3,
       actionButton("theorybutton",
-        HTML("<strong>Theory</strong><br><p> To learn about the theoratical
+        HTML("<strong>Theory</strong><br><p> To learn about the theoretical
              <br> background of the experimental <br> paradigms and the TWIN
              </p>"),icon("book"), style = "background-color: #ffff99",
              width="300px")),
         column(3,
       actionButton("parambutton",
         HTML("<strong>Parameters</strong><br><p> To play around and visualize
-             <br> the model parameters in <br> the Focused Attention Paradigm
-             </p>"),icon("area-chart"), style = "background-color: #5fdc5f",
+             <br> the model predictions of <br> the Focused Attention Paradigm
+             </p>"), icon("area-chart"), style = "background-color: #5fdc5f",
              width="300px"))),
       fluidRow(
         column(3, offset=3,
       actionButton("simbutton",
         HTML("<strong>Simulation</strong> <br><p>To simulate virtual data using
-             different <br> start parameters and SOAs for both <br>
-             Paradigms</p>"), icon("dashboard"), style="background-color:
+             different <br> parameter values for both <br>
+             paradigms</p>"), icon("dashboard"), style="background-color:
              #ed3f40",width="300px")),
         column(3,
       actionButton("estbutton",
         HTML("<strong>Estimation</strong> <br> <p>To estimate the parameters
-             either from <br> previously created data (Simulation), <br> or
+             either from <br> previously created data (simulation), <br> or
              your own data</p>"), icon("paper-plane"), style="background-color:
              #2f84ff", width="300px"))),
         # adding footer: <div class="footer">Footer text</div>
         tags$div(class = "footer",
+          fluidRow(
+          column(6,
+            p(class="text-info", "This project is part of",
+              a(href="https://tquant.eu/", target="_blank",
+             img(src="tquant100.png", width = "30%")), align="left")),
+          column(6,
                  p("Contact us on", a(icon("github"),"Github",
-                   href ="https://github.com/Kaanwoj/shinyTWIN")))
-    ),
+                   href ="https://github.com/Kaanwoj/shinyTWIN"))))
+    )),
 
       source(file.path("ui", "ui_Theory.R"), local = TRUE)$value,
 
@@ -105,7 +111,7 @@ shinyUI(
                         min = 100, max = 500, value = 200),
             sliderInput("sd_second","2nd stage standard deviation",
                         min = 0, max = 100, value = 50),
-            sliderInput("delta","Amount of integration (\\(\\delta\\))",
+            sliderInput("delta","Amount of integration (\\(\\Delta\\))",
                         min = -300, max = 300, value = 100),
             sliderInput("omega","Window width (\\(\\omega\\))",
                         min = 0, max = 500, value = 200)),
