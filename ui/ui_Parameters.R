acf01729c41dfc41872792cb6735bad4b103f008
Katharina Naumann <katharina.naumann@uni-tuebingen.de>
2017-09-14 17:17:14 +0200

    Remove Parameter info, because now we have legends

--- ui/ui_Parameters.R
+++ ui/ui_Parameters.R
@@ -12,13 +12,15 @@ tabPanel("Parameters", value = "Para",
     column(10,
       # exponential distribution
       conditionalPanel(condition = ("input.distPar == 'expFAP'"),
-      fluidRow(
-        column(5,
-        sliderInput("mu_t","Mean for target stimulus (\\(\\frac{1}{\\lambda_V}\\))",
-                    min = 1, max = 100, value = 100)),
-        column(5,
-        sliderInput("mu_nt", "Mean for non-target stimulus (\\(\\frac{1}{\\lambda_A}\\))",
-                    min = 1, max = 100, value = 50)))),
+        fluidRow(
+          column(5,
+            sliderInput("mu_t","Mean for target stimulus
+                        (\\(\\frac{1}{\\lambda_V}\\))", min = 1, max = 100,
+                        value = 100)),
+          column(5,
+            sliderInput("mu_nt", "Mean for non-target stimulus
+                        (\\(\\frac{1}{\\lambda_A}\\))", min = 1, max = 100,
+                        value = 50)))),
       # normal distribution
       conditionalPanel( condition = ("input.distPar == 'normFAP'"),
         fluidRow(
@@ -43,49 +45,29 @@ tabPanel("Parameters", value = "Para",
           column(4,
             sliderInput("range_s2","Range for non-target stimulus",
                     min = 1, max = 300, value = c(70,150)))))
-      )),
+    )),
     h5("Second stage processing time (in ms)"),
     fluidRow(
-    column(3,
+      column(3,
         sliderInput("mu_second","Mean (\\(\\mu\\))",
                     min = 100, max = 500, value = 200)),
-    column(3,
+      column(3,
         sliderInput("sd_second","Standard deviation",
                     min = 0, max = 100, value = 50)),
-    column(3,
+      column(3,
         sliderInput("delta","Amount of integration (\\(\\Delta\\))",
                     min = -300, max = 300, value = 100)),
-    column(3,
+      column(3,
         sliderInput("omega","Window width (\\(\\omega\\))",
                     min = 0, max = 500, value = 200))),
-      fluidRow(
-        column(4,
-               plotOutput("uni_data_t")),
-        column(4,
-               plotOutput("prob")),
-        column(4,
-               plotOutput("data"))),
-               ########### Parameters Tab Description #########
-               br(),
-               tags$div(class = "help-tip", tags$p("This tab shows you the
-                   different dependencies of the model parameters.")),
-               # <div class="help-tip">
-               # <p>Help tip text blabla.</p>
-               # </div>)
-               br(),
-               tags$ol(
-                 tags$li("The topleft graph shows the exponential
-                         distribution of the intensity measure lambda for
-                         both", span("visual (target)", style =
-                         "color:red"), "and", span("auditory (non-target)
-                         stimuli.", style = "color:blue")),
-                 tags$li("The topright graph shows the mean RTs of the
-                         different SOAs for both the", span("unimodal",
-                         style = "color:blue"), "and", span("bimodal",
-                         style = "color:red"), "task condition. \n A
-                         decrease in RTs in the bimodal task condition
-                         compared to the unimodal condition implies
-                         fascilitation."),
-                 tags$li("The bottom graph displays the probability of
-                         integration depending on the length of the SOA."))
+    fluidRow(
+      column(4,
+        plotOutput("uni_data_t")),
+      column(4,
+        plotOutput("prob")),
+      column(4,
+        tags$div(class = "help-tip",
+          p("A decrease in RTs in the bimodal task condition compared to the
+            unimodal condition implies facilitation.")),
+        plotOutput("data")))
 )
